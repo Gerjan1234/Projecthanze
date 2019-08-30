0f604b2eebd8b6e56e919b475ff6d84ebf49529c
Gerjan <gkruize@gmail.com>
2019-08-30 21:32:19 +0200

    code vereenvoudigd

--- Frontend/upload.js
+++ Frontend/upload.js
@@ -33,33 +33,26 @@ console.log(last); //
     success: function(data){
     var data2 = JSON.parse(data);
     console.log(data);
-//loop om tabel te vullen
-var a = 0;
-var b = 1;
-var c = 2;
-var d = 3;
-var e = 4;
-var f = 5;
-var g = 6;
-var h = 7;
-var i = 8;
-var j = 9;
-for (var k = 0; k < data2.length; (k=k+10)) {
-var $newListItem = $('<tr>' + '<td id=' + data2[a].goedfout + ' title=' + data2[a].format + '>' + data2[a].waarde + '</td>' + '<td id=' + data2[b].goedfout + ' title=' + data2[b].format + '>' + data2[b].waarde + '</td>' + '<td id=' + data2[c].goedfout + ' title=' + data2[c].format + '>' + data2[c].waarde + '</td>' + '<td id=' + data2[d].goedfout + ' title=' + data2[d].format + '>' + data2[d].waarde + '</td>' + '</td>' +
-'<td id=' + data2[e].goedfout + ' title=' + data2[e].format + '>' + data2[e].waarde + '</td>' + '<td id=' + data2[f].goedfout + ' title=' + data2[f].format + '>' + data2[f].waarde + '</td>' + '<td id=' + data2[g].goedfout + ' title=' + data2[g].format + '>' + data2[g].waarde + '</td>' + '<td id=' + data2[h].goedfout + ' title=' + data2[h].format + '>' + data2[h].waarde + '</td>' + '<td id=' + data2[i].goedfout + ' title=' + data2[i].format + '>' + data2[i].waarde + '</td>' + '<td id=' + data2[j].goedfout + ' title=' + data2[j].format + '>' + data2[j].waarde + '</td>' + '<tr>');
-    $('tr:last').after($newListItem);
-    a=a+10;
-    b=b+10;
-    c=c+10;
-    d=d+10;
-    e=e+10;
-    f=f+10;
-    g=g+10;
-    h=h+10;
-    i=i+10;
-    j=j+10;
-//loop om met kleur aan te geven of een cel groen of rood is
-  }
+
+  console.log(data2.length);//=30
+  var q = 0 //begin bij 0
+  var r = 10 //r = aantal kolomen
+  var aantalregels = (data2.length / r);  //delen door aantal kolomen
+  //loop door een  regel
+    for (var b = 0; b < aantalregels; b++) {
+      var tabel = "";
+    //loop door items van een  regel
+        for (var l = q; l < r; l++) {
+          var colom = '<td id=' + data2[l].goedfout + ' title=' + data2[l].format + '>' + data2[l].waarde + '</td>'
+          var tabel = tabel + colom  //maak van cellen een regel
+        }
+      var $newListItem = $('<tr>' + tabel + '<tr>'); $('tr:last').after($newListItem);   //zet de regels in tabel.
+      var $newListItem = "";
+    var q = l;  //waarde voor de volgende regel
+    var r = r + 10; //waarde voor volgene regel
+    }
+
+//loop voor de tabel van kleur te voor zien
   var table = document.getElementById("datatabel"); // Get the table
   for (var i = 1, row; row = table.rows[i]; i++) {        //loop door de rijen begin  in rij 2
       for (var j = 0, cell; cell = row.cells[j]; j++) {   //loop door de cellen
