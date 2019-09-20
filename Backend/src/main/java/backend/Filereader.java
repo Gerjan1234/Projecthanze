efb5e94a76cd36bdb839222fbc9db6412a3fc5ad
Gerjan <gkruize@gmail.com>
2019-09-20 23:31:49 +0200

    aanpasing data naar database

--- Backend/src/main/java/backend/Filereader.java
+++ Backend/src/main/java/backend/Filereader.java
@@ -125,19 +125,64 @@ public class Filereader {
                                         test2.format = "getal_5_cijvers";
                                     }
                                     break;
-                                case 2: //String
+                                case 2: //double
+                                    try {
+                                        Double.parseDouble(test[j]);
+                                        test2.regel = regel;
+                                        test2.type = "invoice_id";
+                                        test2.goedfout = true;
+                                        test2.waarde = test[j];
+                                    } catch (NumberFormatException e) {
+                                        test2.regel = regel;
+                                        test2.type = "invoice_id";
+                                        test2.goedfout = false;
+                                        test2.waarde = test[j];
+                                        test2.format = "getal";
+                                    }
+                                    break;
+                                case 3: //float
+                                    try {
+                                        Float.parseFloat(test[j]);
+                                        test2.regel = regel;
+                                        test2.type = "salary";
+                                        test2.goedfout = true;
+                                        test2.waarde = test[j];
+                                    } catch (NumberFormatException e) {
+                                        test2.regel = regel;
+                                        test2.type = "salary";
+                                        test2.goedfout = false;
+                                        test2.waarde = test[j];
+                                        test2.format = "getal met punt";
+                                    }
+                                    break;
+                                case 4: //float
+                                    try {
+                                        Float.parseFloat(test[j]);
+                                        test2.regel = regel;
+                                        test2.type = "parttime_factor";
+                                        test2.goedfout = true;
+                                        test2.waarde = test[j];
+                                    } catch (NumberFormatException e) {
+                                        test2.regel = regel;
+                                        test2.type = "parttime_factor";
+                                        test2.goedfout = false;
+                                        test2.waarde = test[j];
+                                        test2.format = "getal met punt";
+                                    }
+                                    break;
+                                case 5: //String
                                     test2.regel = regel;
                                     test2.type = "first_name";
                                     test2.goedfout = true;
                                     test2.waarde = test[j];
                                     break;
-                                case 3: //String
+                                case 6: //String
                                     test2.regel = regel;
                                     test2.type = "last_name";
                                     test2.goedfout = true;
                                     test2.waarde = test[j];
                                     break;
-                                case 4: //date
+                                case 7: //date
                                     try {
                                         new SimpleDateFormat("dd/MM/yyyy").parse(test[j]);
                                         test2.regel = regel;
@@ -152,19 +197,19 @@ public class Filereader {
                                         test2.format = "formaat_dd/MM/yyyy";
                                     }
                                     break;
-                                case 5: //String
+                                case 8: //String
                                     test2.regel = regel;
                                     test2.type = "status";
                                     test2.goedfout = true;
                                     test2.waarde = test[j];
                                     break;
-                                case 6: //String
+                                case 9: //String
                                     test2.regel = regel;
                                     test2.type = "gender";
                                     test2.goedfout = true;
                                     test2.waarde = test[j];
                                     break;
-                                case 7: //int
+                                case 10: //int
                                     try {
                                         Integer.parseInt(test[j]);
                                         test2.regel = regel;
@@ -180,13 +225,13 @@ public class Filereader {
                                         test2.format = "getal_4_cijvers";
                                     }
                                     break;
-                                case 8: //String
+                                case 11: //String
                                     test2.regel = regel;
                                     test2.type = "communication_type";
                                     test2.goedfout = true;
                                     test2.waarde = test[j];
                                     break;
-                                case 9: //date
+                                case 12: //date
                                     try {
                                         new SimpleDateFormat("dd/MM/yyyy").parse(test[j]);
                                         test2.regel = regel;
