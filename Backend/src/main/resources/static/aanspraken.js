ba45a45d12d65b0918704803d1f16c20c9ffbad9
TjoRepo <T.jo@live.nl>
2019-09-27 10:40:56 +0200

    aanspraken geautomatiseerd

--- Backend/src/main/resources/static/aanspraken.js
+++ Backend/src/main/resources/static/aanspraken.js
@@ -1,3 +1,55 @@
+$(document).ready(function() {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8080/werkgevers',
+        dataType: "json",
+        async:false,
+        success: function(data) {
+
+            console.log(data);
+
+   	//var txt = [data]; //zet data in een array
+            var aanspraken_data = '';
+                $.each(data, function(key, value){
+                  aanspraken_data += '<tr>';
+                  aanspraken_data += '<td>'+value.employerId+'</td>';
+                  aanspraken_data += '<td>'+value.companyName+'</td>';
+                  aanspraken_data += '<tr>';
+                });
+                $('#aanspraken_table').append(aanspraken_data);
+         		}
+         });
+     })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //$(document).ready(function() {
 //    $.ajax({
 //        type: 'GET',
@@ -15,36 +67,19 @@
 //
 //});
 
-function getAanspraken() {
-    $.ajax({
-        type: 'GET',
-        url: 'http://localhost:8080/werkgevers',
-        dataType: "json",
-        async:false,
-        success: function(data) {
-
-            console.log(data);
-
-            document.getElementById("aansprakenOutput").value = JSON.stringify(data);
-
-        },
-        error: function (jqXHR, exception) {
-                     console.log("fout:" + exception)
-          }
-    });
-}
 
 
-function MakeTabel() {
-
-}
-    var $newListItem = $('<tr>' + tabel + '<tr>'); $('tr:last').after($newListItem);   //zet de regels in tabel..
-      $newListItem.show();
-//retour data op scherm in tabel zetten
-  var q = 0 //begin bij 0
-if(max == true){
-  var r = 13 //r = aantal kolomen
-}else {
-  var r = 5
-}
+//
+//function MakeTabel() {
+//
+//}
+//    var $newListItem = $('<tr>' + tabel + '<tr>'); $('tr:last').after($newListItem);   //zet de regels in tabel..
+//      $newListItem.show();
+////retour data op scherm in tabel zetten
+//  var q = 0 //begin bij 0
+//if(max == true){
+//  var r = 13 //r = aantal kolomen
+//}else {
+//  var r = 5
+//}
 
